d80b51ce942e59b248c17600cfafc63c747b98ac
Alicia <aliciasanchez11614@gmail.com>
2025-11-04 15:24:17 -0800

    backend incidents.js

--- packages/express-backend/incidents.js
+++ packages/express-backend/incidents.js
@@ -2,21 +2,34 @@ import mongoose from "mongoose";
 
 const IncidentSchema = new mongoose.Schema(
     {
-        incidentID: {
+        incidentId: {
             type: String,
             unique: true,
+        },
+        title: {
+            type: String,
+            //Untitled placeholder in frontend
+        },
+        status: {
+            type: String,
             trim: true,
+            required: true,
         },
-        name: {
+        description: {
             type: String,
-            default: "Untitled",
+            trim: true,
+            //add description placeholder in frontend
         },
-        created: {
-            type: String,
-
+        tags: {
+            //Stored as an array
+            type: [String],
+            default: [],
         }
     },
-    {collection: "incidents"}
+    {
+        collection: "incidents",
+        timestamps: true
+    }
 );
 
 //Generate random unique ID
@@ -31,12 +44,12 @@ IncidentSchema.pre("save", async function (next){
     while (!unique){
         //generates random 5 digit number
         const randNum = Math.floor(Math.random() * 90000) + 10000
-        const newID = `event-${randNum}`;
+        const newId = `event-${randNum}`;
         
         //Making sure it's unique
-        const existing = await mongoose.models.Incident.findOne({IncidentID: newID});
+        const existing = await mongoose.models.Incident.findOne({incidentId: newId});
         if (!existing) {
-            doc.IncidentID = newID;
+            doc.incidentID = newId;
             unique = true;
         }
     }
